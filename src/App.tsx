2dd611c7af6a3851eda065cf55876364fa4be134
James Conlon <james.conlon@fr.fcm.travel>
2023-12-02 14:12:52 +0100

    add style for overall layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,24 @@ import UnorderedList from './UnorderedList';
 const App = () => {
 	return (
 		<>
-			<h1>Business Model Canvas</h1>
-			<div className="app-container">
-				<UnorderedList title={'Key Partners'} />
-				<UnorderedList title={'Key Partners'} />
+			<div className="stack">
+				<div className="row">
+					<UnorderedList title={'Key Partners'} />
+					<div className="stack">
+						<UnorderedList title={'Key Activities'} />
+						<UnorderedList title={'Key Resources'} />
+					</div>
+					<UnorderedList title={'Value Propositions'} />
+					<div className="stack">
+						<UnorderedList title={'Customer Relationships'} />
+						<UnorderedList title={'Channels'} />
+					</div>
+					<UnorderedList title={'Customer Segments'} />
+				</div>
+				<div className="row">
+					<UnorderedList title={'Cost Structure'} />
+					<UnorderedList title={'Revenue Streams'} />
+				</div>
 			</div>
 		</>
 	);
